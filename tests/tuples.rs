fe98514f33f75b690cdbad9e0981c9b3bfbd83aa
Lukasz Janyst <lukasz@jany.st>
2022-07-28 16:33:07 +0200

    tests: Test color operations

--- tests/tuples.rs
+++ tests/tuples.rs
@@ -1,4 +1,4 @@
-use ray_tracer::{feq, point, vector, Tuple};
+use ray_tracer::{color, feq, point, vector, Tuple};
 
 #[test]
 fn tuple_point() {
@@ -132,4 +132,40 @@ fn vector_cross_product() {
     let b = vector(2.0, 3.0, 4.0);
     assert_eq!(a.cross(&b), vector(-1.0, 2.0, -1.0));
     assert_eq!(b.cross(&a), vector(1.0, -2.0, 1.0));
+}
+
+#[test]
+fn create_color() {
+    let c = color(-0.5, 0.4, 1.7);
+    assert_eq!(c.r(), -0.5);
+    assert_eq!(c.g(), 0.4);
+    assert_eq!(c.b(), 1.7);
+}
+
+#[test]
+fn add_colors() {
+    let c1 = color(0.9, 0.6, 0.75);
+    let c2 = color(0.7, 0.1, 0.25);
+    assert_eq!(c1 + c2, color(1.6, 0.7, 1.0));
+}
+
+#[test]
+fn subtract_colors() {
+    let c1 = color(0.9, 0.6, 0.75);
+    let c2 = color(0.7, 0.1, 0.25);
+    assert_eq!(c1 - c2, color(0.2, 0.5, 0.5));
+}
+
+#[test]
+fn multiply_color_by_scalar() {
+    let c = color(0.2, 0.3, 0.4);
+    assert_eq!(2.0 * c, color(0.4, 0.6, 0.8));
+    assert_eq!(c * 2.0, color(0.4, 0.6, 0.8));
+}
+
+#[test]
+fn multiply_colors() {
+    let c1 = color(1.0, 0.2, 0.4);
+    let c2 = color(0.9, 1.0, 0.1);
+    assert_eq!(c1.hadamard(&c2), color(0.9, 0.2, 0.04));
 }
