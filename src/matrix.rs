345a557d2fa611e8cd148777427ae9a17ec981f2
Lukasz Janyst <lukasz@jany.st>
2022-07-29 13:48:34 +0200

    matrix: Implement computation of determinants

--- src/matrix.rs
+++ src/matrix.rs
@@ -9,6 +9,7 @@ use std::ops::Mul;
 
 #[derive(Debug, Copy, Clone)]
 pub struct Matrix {
+    size: usize,
     data: [f64; 16],
 }
 
@@ -32,6 +33,7 @@ impl Matrix {
         m33: f64,
     ) -> Matrix {
         Self {
+            size: 4,
             data: [
                 m00, m01, m02, m03, //
                 m10, m11, m12, m13, //
@@ -43,11 +45,12 @@ impl Matrix {
 
     pub fn new2(m00: f64, m01: f64, m10: f64, m11: f64) -> Matrix {
         Self {
+            size: 2,
             data: [
                 m00, m01, 0.0, 0.0, //
                 m10, m11, 0.0, 0.0, //
-                0.0, 0.0, 1.0, 0.0, //
-                0.0, 0.0, 0.0, 1.0, //
+                0.0, 0.0, 0.0, 0.0, //
+                0.0, 0.0, 0.0, 0.0, //
             ],
         }
     }
@@ -64,17 +67,19 @@ impl Matrix {
         m22: f64,
     ) -> Matrix {
         Self {
+            size: 3,
             data: [
                 m00, m01, m02, 0.0, //
                 m10, m11, m12, 0.0, //
                 m20, m21, m22, 0.0, //
-                0.0, 0.0, 0.0, 1.0, //
+                0.0, 0.0, 0.0, 0.0, //
             ],
         }
     }
 
     pub fn zero() -> Matrix {
         Self {
+            size: 4,
             data: [
                 0.0, 0.0, 0.0, 0.0, //
                 0.0, 0.0, 0.0, 0.0, //
@@ -86,6 +91,7 @@ impl Matrix {
 
     pub fn one() -> Matrix {
         Self {
+            size: 4,
             data: [
                 1.0, 0.0, 0.0, 0.0, //
                 0.0, 1.0, 0.0, 0.0, //
@@ -96,7 +102,7 @@ impl Matrix {
     }
 
     pub fn at(&self, i: usize, j: usize) -> f64 {
-        check_bounds(i, j);
+        check_bounds(i, j, self.size);
         self.data[idx(i, j)]
     }
 
@@ -109,17 +115,68 @@ impl Matrix {
         }
         res
     }
+
+    pub fn det(&self) -> f64 {
+        if self.size == 2 {
+            return self.at(0, 0) * self.at(1, 1) - self.at(0, 1) * self.at(1, 0);
+        }
+        let mut d = 0.0;
+        for i in 0..self.size {
+            d += self.data[idx(0, i)] * self.cofactor(0, i);
+        }
+        d
+    }
+
+    /// Determinant of the submatrix with row i and column j removed
+    pub fn minor(&self, i: usize, j: usize) -> f64 {
+        let m = self.submatrix(i, j);
+        m.det()
+    }
+
+    pub fn cofactor(&self, i: usize, j: usize) -> f64 {
+        let m = self.minor(i, j);
+        if (i + j) % 2 == 1 {
+            return -m;
+        }
+        m
+    }
+
+    /// Create a copy of the current matrix with row i and column j removed
+    pub fn submatrix(&self, i: usize, j: usize) -> Matrix {
+        check_bounds(i, j, self.size);
+        let mut res = Matrix::zero();
+        res.size = self.size - 1;
+
+        let mut i2 = 0_usize;
+
+        for i1 in 0..self.size {
+            if i1 == i {
+                continue;
+            }
+
+            let mut j2 = 0_usize;
+            for j1 in 0..self.size {
+                if j1 == j {
+                    continue;
+                }
+                res.data[idx(i2, j2)] = self.data[idx(i1, j1)];
+                j2 += 1;
+            }
+            i2 += 1;
+        }
+        res
+    }
 }
 
 fn idx(i: usize, j: usize) -> usize {
     j + 4 * i
 }
 
-fn check_bounds(i: usize, j: usize) {
-    if i >= 4 {
+fn check_bounds(i: usize, j: usize, dims: usize) {
+    if i >= dims {
         panic!("i is out of bounds: {}", i);
     }
-    if j >= 4 {
+    if j >= dims {
         panic!("j is out of bounds: {}", j);
     }
 }
