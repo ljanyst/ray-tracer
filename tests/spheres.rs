c05e62ef9c5a9c8bb18d6a58fb6b1798233bd48a
Lukasz Janyst <lukasz@jany.st>
2022-07-30 16:48:26 +0200

    tests: Update intersection tests to use the intersection tracker

--- tests/spheres.rs
+++ tests/spheres.rs
@@ -1,49 +1,51 @@
-use ray_tracer::{feq, point, vector, Ray, Sphere};
+use ray_tracer::{feq, intersect, peq, point, vector, Ray, Shape, Sphere};
 
 #[test]
 fn intersect_ray_and_sphere() {
     let r = Ray::new(point(0.0, 0.0, -5.0), vector(0.0, 0.0, 1.0));
-    let s = Sphere::new();
-    let xs = s.intersect(&r);
+    let s = Box::new(Sphere::new()) as Box<dyn Shape>;
+    let xs = intersect(&s, &r);
     assert_eq!(xs.len(), 2);
-    assert!(feq(xs[0], 4.0));
-    assert!(feq(xs[1], 6.0));
+    assert!(feq(xs.at(0).t(), 4.0));
+    assert!(peq(xs.at(0).shape(), &s));
+    assert!(feq(xs.at(1).t(), 6.0));
+    assert!(peq(xs.at(1).shape(), &s));
 }
 
 #[test]
 fn intersect_ray_and_sphere_at_tangent() {
     let r = Ray::new(point(0.0, 1.0, -5.0), vector(0.0, 0.0, 1.0));
-    let s = Sphere::new();
-    let xs = s.intersect(&r);
+    let s = Box::new(Sphere::new()) as Box<dyn Shape>;
+    let xs = intersect(&s, &r);
     assert_eq!(xs.len(), 2);
-    assert!(feq(xs[0], 5.0));
-    assert!(feq(xs[1], 5.0));
+    assert!(feq(xs.at(0).t(), 5.0));
+    assert!(feq(xs.at(1).t(), 5.0));
 }
 
 #[test]
 fn intersect_ray_and_sphere_no_intersection() {
     let r = Ray::new(point(0.0, 2.0, -5.0), vector(0.0, 0.0, 1.0));
-    let s = Sphere::new();
-    let xs = s.intersect(&r);
+    let s = Box::new(Sphere::new()) as Box<dyn Shape>;
+    let xs = intersect(&s, &r);
     assert_eq!(xs.len(), 0);
 }
 
 #[test]
 fn intersect_ray_and_sphere_origin_inside() {
     let r = Ray::new(point(0.0, 0.0, 0.0), vector(0.0, 0.0, 1.0));
-    let s = Sphere::new();
-    let xs = s.intersect(&r);
+    let s = Box::new(Sphere::new()) as Box<dyn Shape>;
+    let xs = intersect(&s, &r);
     assert_eq!(xs.len(), 2);
-    assert!(feq(xs[0], -1.0));
-    assert!(feq(xs[1], 1.0));
+    assert!(feq(xs.at(0).t(), -1.0));
+    assert!(feq(xs.at(1).t(), 1.0));
 }
 
 #[test]
 fn intersect_ray_and_sphere_behind() {
     let r = Ray::new(point(0.0, 0.0, 5.0), vector(0.0, 0.0, 1.0));
-    let s = Sphere::new();
-    let xs = s.intersect(&r);
+    let s = Box::new(Sphere::new()) as Box<dyn Shape>;
+    let xs = intersect(&s, &r);
     assert_eq!(xs.len(), 2);
-    assert!(feq(xs[0], -6.0));
-    assert!(feq(xs[1], -4.0));
+    assert!(feq(xs.at(0).t(), -6.0));
+    assert!(feq(xs.at(1).t(), -4.0));
 }
