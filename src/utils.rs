b96350a00f98aa779f530f7e98736564a60a423a
Lukasz Janyst <lukasz@jany.st>
2022-07-30 16:45:25 +0200

    utils: Implement pointer comparison

--- src/utils.rs
+++ src/utils.rs
@@ -6,3 +6,9 @@ use crate::constants::EPSILON;
 pub fn feq(a: f64, b: f64) -> bool {
     (a - b).abs() < EPSILON
 }
+
+pub fn peq<T: ?Sized>(left: &Box<T>, right: &Box<T>) -> bool {
+    let left: *const T = left.as_ref();
+    let right: *const T = right.as_ref();
+    left == right
+}
