632e3e5a57c234d6880c75137a33bced1101757c
Lukasz Janyst <lukasz@jany.st>
2022-07-30 21:03:41 +0200

    shape: Add transform and current_transform functions

--- src/shape.rs
+++ src/shape.rs
@@ -1,8 +1,11 @@
 // Copyright 2022 Lukasz Janyst <lukasz@jany.st>
 // Licensed under the MIT license, see the LICENSE file for details.
 
+use crate::matrix::Matrix;
 use crate::ray::Ray;
 
 pub trait Shape {
     fn intersect(&self, ray: &Ray) -> Vec<f64>;
+    fn transform(&mut self, transform: Matrix);
+    fn current_transform(&self) -> &Matrix;
 }
