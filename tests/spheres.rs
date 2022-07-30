e24c33d1248d646b89ccc3a0c17e39fbeca9f9f0
Lukasz Janyst <lukasz@jany.st>
2022-07-30 21:12:05 +0200

    tests: Test sphere transformations

--- tests/spheres.rs
+++ tests/spheres.rs
@@ -1,4 +1,5 @@
 use ray_tracer::{feq, intersect, peq, point, vector, Ray, Shape, Sphere};
+use ray_tracer::{scaling, translation, Matrix};
 
 #[test]
 fn intersect_ray_and_sphere() {
@@ -48,4 +49,38 @@ fn intersect_ray_and_sphere_behind() {
     assert_eq!(xs.len(), 2);
     assert!(feq(xs[0].t(), -6.0));
     assert!(feq(xs[1].t(), -4.0));
+}
+
+#[test]
+fn construct_unit_sphere() {
+    let s = Sphere::unit();
+    assert_eq!(*s.current_transform(), Matrix::one());
+}
+
+#[test]
+fn move_unit_sphere() {
+    let mut s = Sphere::unit();
+    let t = translation(2.0, 3.0, 4.0);
+    s.transform(t);
+    assert_eq!(*s.current_transform(), t);
+}
+
+#[test]
+fn intersect_ray_and_scaled_sphere() {
+    let r = Ray::new(point(0.0, 0.0, -5.0), vector(0.0, 0.0, 1.0));
+    let mut s = Box::new(Sphere::unit()) as Box<dyn Shape>;
+    s.transform(scaling(2.0, 2.0, 2.0));
+    let xs = intersect(&s, &r);
+    assert_eq!(xs.len(), 2);
+    assert!(feq(xs[0].t(), 3.0));
+    assert!(feq(xs[1].t(), 7.0));
+}
+
+#[test]
+fn intersect_ray_and_translated_sphere() {
+    let r = Ray::new(point(0.0, 0.0, -5.0), vector(0.0, 0.0, 1.0));
+    let mut s = Box::new(Sphere::unit()) as Box<dyn Shape>;
+    s.transform(translation(5.0, 0.0, 0.0));
+    let xs = intersect(&s, &r);
+    assert_eq!(xs.len(), 0);
 }
