3bf03076838c4683803cbaf795168434ad2c2aa7
Lukasz Janyst <lukasz@jany.st>
2022-08-01 14:10:54 +0200

    shape: Add the normal_at method to the trait

--- src/shape.rs
+++ src/shape.rs
@@ -3,9 +3,11 @@
 
 use crate::matrix::Matrix;
 use crate::ray::Ray;
+use crate::tuple::Tuple;
 
 pub trait Shape {
     fn intersect(&self, ray: &Ray) -> Vec<f64>;
     fn transform(&mut self, transform: Matrix);
     fn current_transform(&self) -> &Matrix;
+    fn normal_at(&self, point_w: Tuple) -> Tuple;
 }
