8285ec81cfedb21803e80d8994a26a1152487004
Lukasz Janyst <lukasz@jany.st>
2022-08-01 11:40:56 +0200

    demo: Implement demo4 - a simple ray caster

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+// Copyright 2022 Lukasz Janyst <lukasz@jany.st>
+// Licensed under the MIT license, see the LICENSE file for details.
+
+use crate::demo::demo4::demo4;
+
+mod demo {
+    pub mod demo4;
+}
+
+pub fn main() {
+    demo4();
+}
