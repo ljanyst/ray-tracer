e1ef0f009d23785250e8945666b0b783abc3ad49
Lukasz Janyst <lukasz@jany.st>
2022-08-01 18:14:31 +0200

    tests: Tesh material shading

--- tests/materials.rs
+++ tests/materials.rs
@@ -1,4 +1,4 @@
-use ray_tracer::{color, Material};
+use ray_tracer::{color, point, point_light, vector, Material};
 
 #[test]
 fn create_default_material() {
@@ -8,4 +8,44 @@ fn create_default_material() {
     assert_eq!(m.diffuse, 0.9);
     assert_eq!(m.specular, 0.9);
     assert_eq!(m.shininess, 200.0);
+}
+
+#[test]
+fn shade_material_light_behind_eye_perpendicular_to_surface() {
+    let m = Material::new();
+    let pos = point(0.0, 0.0, 0.0);
+    let eyev = vector(0.0, 0.0, -1.0);
+    let normalv = vector(0.0, 0.0, -1.0);
+    let light = point_light(point(0.0, 0.0, -10.0), color(1.0, 1.0, 1.0));
+    assert_eq!(
+        m.lighting(&light, &pos, &eyev, &normalv),
+        color(1.9, 1.9, 1.9)
+    );
+}
+
+#[test]
+fn shade_material_light_perpendicular_eye_45_to_surface() {
+    let m = Material::new();
+    let pos = point(0.0, 0.0, 0.0);
+    let sq22 = 2.0_f64.sqrt() / 2.0;
+    let eyev = vector(0.0, sq22, -sq22);
+    let normalv = vector(0.0, 0.0, -1.0);
+    let light = point_light(point(0.0, 0.0, -10.0), color(1.0, 1.0, 1.0));
+    assert_eq!(
+        m.lighting(&light, &pos, &eyev, &normalv),
+        color(1.0, 1.0, 1.0)
+    );
+}
+
+#[test]
+fn shade_material_light_45_eye_perpendicular_to_surface() {
+    let m = Material::new();
+    let pos = point(0.0, 0.0, 0.0);
+    let eyev = vector(0.0, 0.0, -1.0);
+    let normalv = vector(0.0, 0.0, -1.0);
+    let light = point_light(point(0.0, 0.0, 10.0), color(1.0, 1.0, 1.0));
+    assert_eq!(
+        m.lighting(&light, &pos, &eyev, &normalv),
+        color(0.1, 0.1, 0.1)
+    );
 }
