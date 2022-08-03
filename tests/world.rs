aee0918dc2838c7fc175873792837f5b374d1004
Lukasz Janyst <lukasz@jany.st>
2022-08-03 16:18:22 +0200

    tests: Test shadows

--- tests/world.rs
+++ tests/world.rs
@@ -1,4 +1,5 @@
 use ray_tracer::{color, feq, point, point_light, vector, Intersection, Ray, World};
+use ray_tracer::{translation, Sphere};
 
 #[test]
 fn intersect_ray_with_world() {
@@ -65,4 +66,32 @@ fn shade_ray_world_hit_intersection_behind_day() {
     let r = Ray::new(point(0.0, 0.0, 0.75), vector(0.0, 0.0, -1.0));
     let c = w.color_at(&r);
     assert_eq!(c, w.shapes[1].material().color);
+}
+
+#[test]
+fn check_point_in_shadow() {
+    let w = World::default();
+    let p1 = point(10.0, -10.0, 10.0);
+    let p2 = point(-20.0, 20.0, -20.0);
+    let p3 = point(-2.0, 2.0, -2.0);
+
+    assert!(w.is_shadowed(&w.lights[0], p1));
+    assert!(!w.is_shadowed(&w.lights[0], p2));
+    assert!(!w.is_shadowed(&w.lights[0], p3));
+}
+
+#[test]
+fn shade_hit_in_shadow() {
+    let mut w = World::empty();
+    let l = point_light(point(0.0, 0.0, -10.0), color(1.0, 1.0, 1.0));
+    w.lights.push(l);
+    w.shapes.push(Box::new(Sphere::unit()));
+    w.shapes
+        .push(Box::new(Sphere::new(translation(0.0, 0.0, 10.0))));
+
+    let r = Ray::new(point(0.0, 0.0, 5.0), vector(0.0, 0.0, 1.0));
+    let i = Intersection::new(4.0, &w.shapes[1]);
+    let p = i.properties(&r);
+    let c = w.shade_hit(p);
+    assert_eq!(c, color(0.1, 0.1, 0.1));
 }
