81464a42d1cd6365d9619cb6f57770dacfa58633
Lukasz Janyst <lukasz@jany.st>
2022-12-03 10:37:02 +0100

    tests: Test cylinder min and max bounds

--- tests/cylinders.rs
+++ tests/cylinders.rs
@@ -1,4 +1,6 @@
-use ray_tracer::{cylinder_unit, feq, intersect, point, vector, Ray, Tuple};
+use ray_tracer::{
+    cylinder_min_max, cylinder_unit, feq, intersect, point, vector, Ray, Shape, Tuple,
+};
 
 #[test]
 fn intersect_ray_and_cylinder() {
@@ -32,6 +34,20 @@ fn intersect_ray_and_cylinder() {
         }
     }
 
+    let run_tests = |td: Vec<TestData>, c: &dyn Shape| {
+        for t in td.iter() {
+            let r = Ray::new(t.origin, t.direction.normalized());
+            let xs = intersect(c, &r);
+            if t.hit {
+                assert_eq!(xs.len(), 2);
+                assert!(feq(xs[0].t(), t.t1));
+                assert!(feq(xs[1].t(), t.t2));
+            } else {
+                assert_eq!(xs.len(), 0);
+            }
+        }
+    };
+
     let td = vec![
         TestData::new_m(point(1.0, 0.0, 0.0), vector(0.0, 1.0, 0.0)),
         TestData::new_m(point(0.0, 0.0, 0.0), vector(0.0, 1.0, 0.0)),
@@ -47,19 +63,19 @@ fn intersect_ray_and_cylinder() {
     ];
 
     let c = cylinder_unit();
-    for t in td.iter() {
-        let r = Ray::new(t.origin, t.direction.normalized());
-        let xs = intersect(c.as_ref(), &r);
-        if t.hit {
-            assert_eq!(xs.len(), 2);
-            println!("{}", xs[0].t());
-            println!("{}", t.t1);
-            assert!(feq(xs[0].t(), t.t1));
-            assert!(feq(xs[1].t(), t.t2));
-        } else {
-            assert_eq!(xs.len(), 0);
-        }
-    }
+    run_tests(td, c.as_ref());
+
+    let td = vec![
+        TestData::new_m(point(0.0, 1.5, 0.0), vector(0.1, 1.0, 0.0)),
+        TestData::new_m(point(0.0, -3.0, -5.0), vector(0.0, 0.0, 1.0)),
+        TestData::new_m(point(0.0, 0.0, -5.0), vector(0.0, 0.0, 1.0)),
+        TestData::new_m(point(0.0, 2.0, -5.0), vector(0.0, 0.0, 1.0)),
+        TestData::new_m(point(0.0, 1.0, -5.0), vector(0.0, 0.0, 1.0)),
+        TestData::new_h(point(0.0, 1.5, -2.0), vector(0.0, 0.0, 1.0), 1.0, 3.0),
+    ];
+
+    let c = cylinder_min_max(1.0, 2.0);
+    run_tests(td, c.as_ref());
 }
 
 #[test]
